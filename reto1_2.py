01a268ba6933d5cfdfabe0d3a3c2c1e72bd89d8d
Luis Ore Cardenas <luisore24@gmail.com>
2021-04-25 06:49:52 -0500

    SEGUNDA FORMA DE RESOLVER EL RETO 1

---
+++ reto1_2.py
@@ -0,0 +1,41 @@
+
+################################################################################
+################ --- SEGUNDA FORMA DE SOLUCION DE RETO #########################
+################################################################################
+
+try:
+
+    notas = list(range(5))
+
+    i = 0
+    for nota in notas:
+        nota = int(input(f'Ingrese Nota #{i+1}: '))
+        notas[i] = nota
+        i += 1
+
+    promedio = sum(notas)/len(notas)
+    nota_max = max(notas)
+    nota_min = min(notas)
+
+    print ("===================================================")
+
+    print (f'Las notas ingresadas del estudiante son: \n')
+    #IMPRIMIENDO LA LISTA DE NOTAS
+    #print (notas)
+    
+    #IMPRIMIENDO LAS NOTAS CON MEDIANTE UN FOR
+    contador = 0
+    j = 1
+    for num in notas:
+        print(f'Nota {j} : {notas[contador]}')
+        contador += 1
+        j += 1 
+        
+    print (f'El promedio de las {len(notas)} notas ingresadas es : --> {promedio}')
+    print (f'La nota mas alta de las {len(notas)} notas ingresadas es : --> {nota_max}')
+    print (f'La nota mas baja de las {len(notas)} notas ingresadas es : --> {nota_min}')
+
+except Exception as ex:
+    print("Ha ocurrido un inconveniente, codigo de error: " + str(ex))
+
+
